5c3a9753286d5d530a69daab7f7ff8b614f656a4
Nikhil Gupta <nikhigu@adobe.com>
2021-06-13 13:11:20 +0530

    Adds token info

--- contracts/NGToken.sol
+++ contracts/NGToken.sol
@@ -2,9 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract NGToken {
+  string public name = "NG Token";
+  string public symbol = "NGT";
   uint256 public totalSupply;
+
+  mapping(address => uint256) public balanceOf;
 
   constructor() public {
     totalSupply = 1000000;
+    balanceOf[msg.sender] = 1000000;
   }
 }
--- test/NGToken.js
+++ test/NGToken.js
@@ -5,10 +5,18 @@ const NGToken = artifacts.require("NGToken");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("NGToken", function (/* accounts */) {
+contract("NGToken", function (accounts) {
   it("should assert true", async function () {
     await NGToken.deployed();
     return assert.isTrue(true);
+  });
+
+  it("has correct name and symbol", async function() {
+    tokenInstance = await NGToken.deployed();
+    tokenName = await tokenInstance.name();
+    tokenSymbol = await tokenInstance.symbol();
+    assert(tokenName, "NG Token");
+    assert(tokenSymbol, "NGT");
   });
 
   it("sets the total supply upon deployment", async function () {
@@ -16,4 +24,10 @@ contract("NGToken", function (/* accounts */) {
     totalSupply = await tokenInstance.totalSupply();
     assert(totalSupply.toNumber(), 100000);
   });
+
+  it("is owned entirely be creator upon deployment", async function() {
+    tokenInstance = await NGToken.deployed();
+    balance = await tokenInstance.balanceOf(accounts[0]);
+    assert(balance.toNumber(), 100000);
+  })
 });
